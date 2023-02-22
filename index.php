e346a0f03d460c5f8875b3a22c190d407b1924f0
Pietro Giordano <giopi87@gmail.com>
2023-02-22 21:25:35 +0100

    modifiche style

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <title>PHP ToDo List</title>
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
       <link rel="stylesheet" href="css/style.css">
 </head>
 
@@ -14,14 +15,22 @@
 
       <div id="app" class="d-flex justify-content-center">
 
-            <div class="box">
-                  <h1 class="text-white">Todo list</h1>
+            <div class="box mt-3">
+                  <h1 class="display-1 text-secondary-emphasis text-center text-uppercase">Todo list</h1>
 
                   <ul class="list-group">
-                        <li v-for="todo in todoList" class="list-group-item" :class="todo.completed ? 'text-decoration-line-through' : '' ">
+                        <li v-for="todo in todoList" class="list-group-item list-group-item-action d-flex justify-content-between" :class="todo.completed ? 'text-decoration-line-through' : '' ">
                               {{ todo.to_do }}
+                              <button class="btn btn-danger" type="button" id="delete-button">
+                                    <i class="bi bi-trash-fill"></i>
+                              </button>
                         </li>
                   </ul>
+
+                  <div class="input-group mt-3">
+                        <input type="text" class="form-control" placeholder="Inserisci elemento...">
+                        <button class="btn btn-outline-warning" type="button" id="add-button">Inserisci</button>
+                  </div>
             </div>
 
       </div>
